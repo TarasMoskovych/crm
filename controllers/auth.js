81005ec3e3c3a6b0ac0248b5f6bfecd1c3ce97b4
Taras Moskovych <moskovych.taras@gmail.com>
2019-05-25 18:03:47 +0300

    implement authorization functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,50 @@
-module.exports.login = (req, res) => {
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const User = require('./../models/User');
+const keys = require('./../config/keys');
+
+module.exports.login = async(req, res) => {
+  const { email, password } = req.body;
+  const candidate = await User.findOne({ email });
+
+  if (candidate) {
+    const encrypted = bcrypt.compareSync(password, candidate.password);
+
+    if (encrypted) {
+      const token = jwt.sign({ email, userId: candidate._id }, keys.jwt, { expiresIn: 60 * 60 });
+      return res.status(200).json({ token: `Bearer ${token}` });
+    }
+
+    return res.status(401).json({
+      message: 'incorrect password'
+    });
+  }
+
+  res.status(404).json({
+    message: 'user is not found'
+  });
 };
 
-module.exports.register = (req, res) => {
+module.exports.register = async(req, res) => {
+  const { name, email, password } = req.body;
+  const candidate = await User.findOne({ email });
 
+  if (candidate) {
+    return res.status(409).json({
+      message: 'email is used'
+    });
+  }
+
+  const salt = bcrypt.genSaltSync(10);
+  const user = new User({
+    name, email, password: bcrypt.hashSync(password, salt)
+  });
+
+  try {
+    await user.save();
+    res.status(201).json(user);
+  } catch(e) {
+    console.log(e);
+  }
 };
