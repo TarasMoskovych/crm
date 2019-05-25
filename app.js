b7e6d23f81d0d85b56635540c950e383cdfeb7e2
Taras Moskovych <moskovych.taras@gmail.com>
2019-05-25 15:58:31 +0300

    add plugins

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const auth = require('./routes/auth');
 const analytic = require('./routes/analytic');
@@ -7,6 +10,11 @@ const order = require('./routes/order');
 const position = require('./routes/position');
 
 const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api/auth', auth);
 app.use('/api/analytic', analytic);
